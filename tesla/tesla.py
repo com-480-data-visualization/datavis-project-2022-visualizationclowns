675a71ffab797ee35d03688f404491110a053938
Alexander Brokking <alexander.balfors@gmail.com>
2022-03-04 17:05:14 +0100

    Added tesla stock data from 2011-2022

---
+++ tesla/tesla.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# In[ ]:
+
+pip install yfinance
+
+# %%
+
+
+import yfinance as yf
+tesla = yf.Ticker("TSLA")
+
+print(tesla.info)
+
+# %%
+data_df = yf.download("TSLA", start="2011-01-01", end="2022-03-01", period="1d")
+data_df.to_csv('tesla.csv')
+# %%
