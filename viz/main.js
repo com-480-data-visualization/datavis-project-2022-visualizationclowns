5895c2bd6d57e503909fc8fc9f120277c3b16c65
Alexander Brokking <alexander.balfors@gmail.com>
2022-03-10 16:07:26 +0100

    Seperated function calls

--- viz/main.js
+++ viz/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as d3 from "d3";
+import { generateTweetsVsPrice } from "./mainVisualization";
 
 // document.querySelector("#app").innerHTML = `
 //   <h1>Hello Vite!</h1>
@@ -8,59 +9,7 @@ import * as d3 from "d3";
 
 (async () => {
   const crypto = await d3.csv("./assets/coin_Bitcoin.csv");
+  let tweets = await d3.csv("./assets/alltweets.csv");
 
-  let margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width =
-      document.body.getBoundingClientRect().width - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-  const svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  const x = d3
-    .scaleTime()
-    .domain(
-      d3.extent(crypto, function (d) {
-        return new Date(d.Date);
-      })
-    )
-    .range([0, width]);
-  svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  const y = d3
-    .scaleLinear()
-    .domain([
-      0,
-      d3.max(crypto, function (d) {
-        return +d.Close;
-      }),
-    ])
-    .range([height, 0]);
-  svg.append("g").call(d3.axisLeft(y));
-
-  svg
-    .append("path")
-    .datum(crypto)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr(
-      "d",
-      d3
-        .line()
-        .x(function (d) {
-          return x(new Date(d.Date));
-        })
-        .y(function (d) {
-          return y(d.Close);
-        })
-    );
+  generateTweetsVsPrice(tweets, crypto);
 })();
---
+++ viz/mainVisualization.js
@@ -0,0 +1,72 @@
+import "./style.css";
+import * as d3 from "d3";
+
+export const generateTweetsVsPrice = (tweets, crypto) => {
+  tweets = tweets.filter((tweet) =>
+    tweet.tweet.match(new RegExp(`.*([b,B]itcoin|BTC).*`))
+  );
+
+  let margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width =
+      document.body.getBoundingClientRect().width - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  const x = d3
+    .scaleTime()
+    .domain(
+      d3.extent(crypto, function (d) {
+        return new Date(d.Date);
+      })
+    )
+    .range([0, width]);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  const y = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(crypto, function (d) {
+        return +d.Close;
+      }),
+    ])
+    .range([height, 0]);
+  svg.append("g").call(d3.axisLeft(y));
+
+  const mouseLine = svg.append("g").attr("class", "mouse-over-effects");
+
+  mouseLine
+    .append("path")
+    .attr("class", "mouse-line")
+    .style("stroke", "black")
+    .style("stroke-width", "1px")
+    .style("opacity", "0");
+
+  svg
+    .append("path")
+    .datum(crypto)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x(function (d) {
+          return x(new Date(d.Date));
+        })
+        .y(function (d) {
+          return y(d.Close);
+        })
+    );
+};
