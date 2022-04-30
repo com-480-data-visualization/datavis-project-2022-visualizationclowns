1acde34d3010a3cd1c7946dea0bfcfdffb8b6883
Albert Sund Aillet <albert@sundaillet.com>
2022-04-30 23:56:43 +0200

    added relevant tweet plot and function refactor

--- viz/mainVisualization.js
+++ viz/mainVisualization.js
@@ -27,9 +27,7 @@ export const generateTweetsVsPrice = (tweets, crypto) => {
   const x = d3
     .scaleTime()
     .domain(
-      d3.extent(crypto, function (d) {
-        return new Date(d.Date);
-      })
+      d3.extent(crypto, d => (new Date(d.Date)))
     )
     .range([0, width]);
   svg
@@ -41,28 +39,16 @@ export const generateTweetsVsPrice = (tweets, crypto) => {
     .scaleLinear()
     .domain([
       0,
-      d3.max(crypto, function (d) {
-        return +d.Close;
-      }),
+      d3.max(crypto, d => (+d.Close))
     ])
     .range([height, 0]);
   svg.append("g").call(d3.axisLeft(y));
 
-  var brush = d3
-    .brushX()
-    .extent([[0, 0], [width, height]])
-    .on("brush end", brushed);
+  const brushed = (event) => {
+    const selection = event.selection
 
-  svg
-    .append("g")
-    .attr("class", "brush")
-    .call(brush);
-
-  function brushed (event) {
-    const mouse = event.selection
-
-    let xDateMin = x.invert(mouse[0]);
-    let xDateMax = x.invert(mouse[1]);
+    let xDateMin = x.invert(selection[0]);
+    let xDateMax = x.invert(selection[1]);
     tweetsSvg.selectAll(".tweet-box").remove();
     tweets
       .filter((tweet) => {
@@ -78,8 +64,17 @@ export const generateTweetsVsPrice = (tweets, crypto) => {
       .forEach((tweet, index) => {
         addTweetBox(tweet, tweetsSvg);
       });
-    }
+  }
 
+  var brush = d3
+    .brushX()
+    .extent([[0, 0], [width, height]])
+    .on("brush end", brushed);
+
+  svg
+    .append("g")
+    .attr("class", "brush")
+    .call(brush);
 
   svg
     .append("path")
@@ -92,11 +87,23 @@ export const generateTweetsVsPrice = (tweets, crypto) => {
       "d",
       d3
         .line()
-        .x(function (d) {
-          return x(new Date(d.Date));
-        })
-        .y(function (d) {
-          return y(d.Close);
-        })
+        .x(d => x(new Date(d.Date)))
+        .y(d => y(+d.Close))
     );
+      
+    // Plotting the relevant tweets
+    svg
+      .append("g")
+      .selectAll("dot")
+      .data(tweets)
+      .enter()
+      .append("circle")
+      .attr("cx", d => x(new Date(d.created_at)))
+      .attr("cy", d => {
+        const bisect = d3.bisector(d => (new Date(d.Date))).right;
+        const i = bisect(crypto, new Date(d.created_at));
+        return y(+crypto[i].Close);
+      })
+      .attr("r", 3)
+      .style("fill", "red");
 };
