4d031968ff41502c12774743c50019b3c77931ff
Albert Sund Aillet <albert@sundaillet.com>
2022-05-06 17:04:19 +0200

    made function for filter and order

--- viz/ranking.js
+++ viz/ranking.js
@@ -5,33 +5,22 @@ import { addTweetBox } from "./tweet";
 (async () => {
   let tweets = await d3.csv("/alltweets.csv");
 
+  const filter_regex = (regex) => {
+    return (tweet) => {
+      return tweet.tweet.toLowerCase().match(new RegExp(`.*(bitcoin|btc).*`));
+    };
+  };
+  const order = (a, b) => d3.descending(a.nlikes, b.nlikes);
+
   const filtered_tweets = {
-    bitcoin: tweets
-      .filter((tweet) =>
-        tweet.tweet.toLowerCase().match(new RegExp(`.*(bitcoin|btc).*`))
-      )
-      .sort(function (a, b) {
-        return d3.descending(a.nlikes, b.nlikes);
-      }),
-    dogecoin: tweets
-      .filter((tweet) =>
-        tweet.tweet.toLowerCase().match(new RegExp(`.*(doge).*`))
-      )
-      .sort(function (a, b) {
-        return d3.descending(a.nlikes, b.nlikes);
-      }),
-    tesla: tweets
-      .filter((tweet) =>
-        tweet.tweet.toLowerCase().match(new RegExp(`.*(tsla|tesla stock).*`))
-      )
-      .sort(function (a, b) {
-        return d3.descending(a.nlikes, b.nlikes);
-      }),
+    bitcoin: tweets.filter(filter_regex(`.*(bitcoin|btc).*`)).sort(order),
+    dogecoin: tweets.filter(filter_regex(`.*(doge).*`)).sort(order),
+    tesla: tweets.filter(filter_regex(`.*(doge).*`)).sort(order),
   };
 
   const tweetsSvg = d3.select(".tweets-ranking");
-
   const radioSelect = d3.selectAll(".radio");
+
   radioSelect.on("change", (e) => {
     tweetsSvg.selectAll(".tweet-box").remove();
     filtered_tweets[e.target.value].forEach((tweet, index) => {
